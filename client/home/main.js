844d663ce24566f389010c9058d50db39a85e7f7
moKhaled73 <mokhaled732003@gmail.com>
2024-09-14 12:10:09 +0300

    ..

--- client/home/main.js
+++ client/home/main.js
@@ -1,88 +1,84 @@
 function showLinks() {
-    const links = document.querySelector(".links");
-    links.style.display = 'flex';
+  const links = document.querySelector(".links");
+  links.style.display = "flex";
 }
 function hideLinks() {
-    const links = document.querySelector(".links");
-    links.style.display = 'none';
+  const links = document.querySelector(".links");
+  links.style.display = "none";
 }
 
 function navigate() {
-
-    window.location.href = "../model_page/heatmap.html"
+  window.location.href = "../model_page/heatmap.html";
 }
 /*
 scroll
 */
-let btn = document.getElementById('scroll');
+let btn = document.getElementById("scroll");
 window.onscroll = function () {
-    if (scrollY >= 400) {
-        btn.style.display = 'block';
-
-    }
-    else {
-        btn.style.display = 'none';
-
-    }
-}
+  if (scrollY >= 400) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+};
 
 btn.onclick = function () {
-    scroll({
-        left: 0,
-        top: 0,
-        behavior: 'smooth'
-    })
-}
+  scroll({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  });
+};
 
-document.addEventListener('DOMContentLoaded', () => {
-    const services = document.querySelector('.serv');
-    const srv = document.querySelectorAll('.srv');
-    const options = {
-        root: null,
-        threshold: 0.5,
-    };
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                srv.forEach((serv, index) => {
-                    setTimeout(() => {
-                        serv.classList.add('fade-in');
-                    }, index * 300);
-                });
-                observer.unobserve(entry.target);
-            }
+document.addEventListener("DOMContentLoaded", () => {
+  const services = document.querySelector(".serv");
+  const srv = document.querySelectorAll(".srv");
+  const options = {
+    root: null,
+    threshold: 0.5,
+  };
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        srv.forEach((serv, index) => {
+          setTimeout(() => {
+            serv.classList.add("fade-in");
+          }, index * 300);
         });
-    }, options);
+        observer.unobserve(entry.target);
+      }
+    });
+  }, options);
 
-    observer.observe(services);
+  observer.observe(services);
 });
 /*-----------------------------------------*/
-const boxes = document.querySelectorAll('.box');
+const boxes = document.querySelectorAll(".box");
 
 const options = {
-    root: null,
-    threshold: 0.2,
-    rootMargin: '-50px'
+  root: null,
+  threshold: 0.2,
+  rootMargin: "-50px",
 };
 
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('slide');
-            observer.unobserve(entry.target);
-        }
-    });
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("slide");
+      observer.unobserve(entry.target);
+    }
+  });
 }, options);
 
-boxes.forEach(box => {
-    observer.observe(box);
+boxes.forEach((box) => {
+  observer.observe(box);
 });
 
 /* theme Dark */
 
 let btn = document.querySelector(".switch");
 btn.onclick = function () {
-    document.body.classList.toggle("dark-theme");
+  document.body.classList.toggle("dark-theme");
 };
 
 /*
